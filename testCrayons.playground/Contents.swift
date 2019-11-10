62ae6e90ede9d7c98974fdb62937df660b1dd86c
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-11-10 15:47:19 -0500

    implemented colors and tables into the initial view controller.

---
+++ testCrayons.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class Crayon {
+    var name: String
+    var red: Double
+    var green: Double
+    var blue: Double
+    var hex: String
+    init(name: String, red: Double, green: Double, blue: Double, hex: String) {
+        self.name = name
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.hex = hex
+    }
+    
+}
+
+let blue = Crayon(name: "Blue", red: 31, green: 117, blue: 254, hex: "#1F75FE")
+let x: CGFloat = CGFloat(blue.red)
+
